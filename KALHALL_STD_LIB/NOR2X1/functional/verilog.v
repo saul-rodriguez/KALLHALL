23d4821318364036b31e1d80d61d56338c1ef4d2
saul <saul@s2424.it.kth.se>
2021-01-06 00:21:42 +0100

    abstract views created

---
+++ KALHALL_STD_LIB/NOR2X1/functional/verilog.v
@@ -0,0 +1,22 @@
+//Verilog HDL for "KALHALL_STD_LIB", "NOR2X1" "functional"
+
+// type:  
+`timescale 1ns/10ps
+`celldefine
+module NOR2X1 (Y, A, B);
+	output Y;
+	input A, B;
+
+	// Function
+	wire int_fwire_0;
+
+	or (int_fwire_0, A, B);
+	not (Y, int_fwire_0);
+
+	// Timing
+	specify
+		(A => Y) = 0;
+		(B => Y) = 0;
+	endspecify
+endmodule
+`endcelldefine

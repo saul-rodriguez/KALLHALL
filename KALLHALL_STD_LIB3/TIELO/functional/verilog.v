769b312ece08e07d764dac682b7800cd7ad6ace4
saul <saul@s2424.it.kth.se>
2021-01-12 01:56:29 +0100

    added DECAP1 FILL1 TIEHI TIELO. The last ones need to be included in the liberty file.

---
+++ KALLHALL_STD_LIB3/TIELO/functional/verilog.v
@@ -0,0 +1,16 @@
+//Verilog HDL for "KALHALL_STD_LIB", "INVX1" "functional"
+ 
+`timescale 1ns/10ps
+`celldefine
+module TIELO (Y);
+	output Y;
+	//input A;
+
+	// Function
+	buf (Y, 1'b0);
+
+	// Timing
+	specify
+	endspecify
+endmodule
+`endcelldefine
